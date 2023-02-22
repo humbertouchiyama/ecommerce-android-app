7a1c00549bba0f94ded27e3fe7d3dd3f4fd8bd89
Humberto Costa <hcosta@allurion.com>
2023-02-22 15:56:19 -0300

    lecture 8

--- app/src/main/java/com/renarosantos/ecommerceapp/MainActivity.kt
+++ app/src/main/java/com/renarosantos/ecommerceapp/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.renarosantos.ecommerceapp
 
 import android.os.Bundle
-import androidx.annotation.NonNull
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private val adapter = ProductCardListAdapter()
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel : ProductListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,19 +20,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.viewProductList.layoutManager= LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.viewProductList.adapter = adapter
-        updateUI(ProductListViewState.Content((1..3).map {
-            ProductCardViewState("Playstation $it", "This is a nice console! Check it out", "200 US$")
-        }))
+        viewModel.viewState.observe(this) { viewState ->
+            updateUI(viewState)
+        }
+        viewModel.loadProductList()
     }
 
     private fun updateUI(viewState : ProductListViewState) {
         when(viewState){
             is ProductListViewState.Content -> {
+                binding.viewProductList.isVisible = true
                 binding.errorView.isVisible = false
                 binding.loadingView.isVisible = false
                 adapter.setData(viewState.productList)
             }
-            ProductListViewState.Error -> {
+            is ProductListViewState.Error -> {
                 binding.viewProductList.isVisible = false
                 binding.errorView.isVisible = true
                 binding.loadingView.isVisible = false
---
+++ app/src/main/java/com/renarosantos/ecommerceapp/ProductRepository.kt
@@ -0,0 +1,17 @@
+package com.renarosantos.ecommerceapp
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+class ProductRepository {
+
+    suspend fun getProductList() : List<ProductCardViewState>{
+        return withContext(Dispatchers.IO) {
+            delay(2000)
+            (1..3).map {
+                ProductCardViewState("Playstation $it", "This is a nice console! Check it out", "200 US$")
+            }
+        }
+    }
+}
